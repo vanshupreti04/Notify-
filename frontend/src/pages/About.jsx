abb87e6b2e83a97ea3a490bcd29841c886746670
vanshupreti04 <vanshupreti0412@gmail.com>
2025-02-15 10:10:56 +0530

    Update About.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-<<<<<<< Updated upstream
       <h1>About us</h1>
-=======
-<<<<<<< HEAD
-      <h1>About us for today</h1>
-=======
-      <h1>About us</h1>
->>>>>>> 10f6b1660d8803e82cde715c4e43d63daed7ddd3
->>>>>>> Stashed changes
     </div>
   )
 }
